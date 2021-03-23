33a5b3985a66724f02510407f219667665eb185f
Antonis Gkoutzamanis <antonis.gkoutzamanis@pfizer.com>
2021-03-23 14:32:30 +0200

    Initial commit

---
+++ resources/js/app.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+
+//Main pages
+import ExampleComponent from './components/ExampleComponent.vue'
+
+
+const app = new Vue({
+    el: '#app',
+    components: { ExampleComponent }
+});
